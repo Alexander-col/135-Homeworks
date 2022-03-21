131b4af32637efc267f6cc08724af322088030e1
Alexander-col <aexro2221@gmail.com>
2022-03-21 14:48:16 -0400

    added a half version of the pyramid code

---
+++ lab-quizes/pyramid.cpp
@@ -0,0 +1,45 @@
+/*
+Author: Alexander Colotl 
+Course: CSCI-135
+Instructor: Tong Yi
+Assignment: E 8.1 
+*/
+#include <iostream>
+
+std::string pyramid (std::string strr)
+{
+    std::string output = "";
+    std::string phaser = ""; // The phaser is a key word that adds to string while being on a single line
+    int len = strr.length(); // Have to fix the output becuase of the thing tha make it appear only once 
+    
+    for (int i = 0; i != len; i++)
+    {
+        phaser += strr[i];
+        output = output + phaser;
+        output += "\n";
+    }
+    for (int i = len-1; i != 0; i--)
+    {
+
+        output = output + strr.substr(0,i);
+        output += "\n";
+    }
+
+
+
+    return output;
+}
+
+int main()
+{
+    // std::cout << pyramid("pyramid") << std::endl;
+    // std::cout << pyramid("hunter") << std::endl;
+    // std::cout << pyramid("computer") << std::endl;
+    // std::cout << pyramid("science") << std::endl;
+    // std::cout << pyramid("dana") << std::endl;
+    // std::cout << pyramid("milkshake") << std::endl;
+    // std::string input = "";
+    // std::cin >> input;
+    // std::cout << pyramid(input) << std::endl;
+    return 0;
+}
